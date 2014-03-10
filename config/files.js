90bdf3bfcfd607a7b9b56746a0003e08d8b2871b
derailed <fernand.galiana@gmail.com>
2014-03-10 16:12:51 -0600

    Revert "updated conf to coffee. add support for fonts"
    This reverts commit 76248fefbf5ac1638f97ad0d06cc04922e5b859f.

---
+++ config/files.js
@@ -0,0 +1,30 @@
+module.exports = function(lineman) {
+  return {
+    js: {
+      vendor: [
+        "vendor/bower/underscore/underscore.js",
+        "vendor/bower/angular/angular.min.js",
+        "vendor/bower/angular-route/angular-route.min.js",
+        "vendor/bower/angular-resource/angular-resource.min.js",
+        "vendor/js/**/*.js"
+      ],
+      app: [
+        "app/js/app.js",
+        "app/js/**/*.js"
+      ]
+    },
+
+    sass: {
+      compile: {
+        options: {
+          loadPath: [
+            "vendor/bower/foundation/*.scss",
+            "vendor/bower/fontawesome/scss/*.scss",
+            "vendor/bower/bourbon/**/*.scss",
+            "app/css/**/*.scss"
+          ]
+        }
+      }
+    }
+  };
+};
